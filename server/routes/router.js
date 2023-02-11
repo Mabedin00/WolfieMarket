dbbde705622047c0b07c8177b48c49efff8f129d
Tyler <tylhuang@umich.edu>
2023-02-11 16:04:21 -0500

    added routes

---
+++ server/routes/router.js
@@ -0,0 +1,10 @@
+const auth = require('../auth')
+const express = require('express')
+const UserController = require('../controllers/user-controller')
+const router = express.Router()
+
+router.post('/register', UserController.registerUser)
+router.post('/login', UserController.loginUser)
+router.get('logout', UserController.logoutUser)
+router.get('/loggedIn', UserController.getLoggedIn)
+module.exports = router
