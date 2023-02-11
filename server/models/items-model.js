841d79e5bcac92261b1c4dec6ac6bf13934dfae2
Tyler <tylhuang@umich.edu>
2023-02-11 14:06:52 -0500

    finished item schema

--- server/models/items-model.js
+++ server/models/items-model.js
@@ -3,6 +3,14 @@ const Schema = mongoose.Schema
 
 const ItemsSchema = new Schema(
     {
-        id: { type: Number }
-    }
+        id: { type: Number, required: true },
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        price: { type: Number, required: true },
+        picture: { type: String, required: true },
+        location: { type: String, required: true },
+    },
+    { timestamps: true },
 )
+
+module.exports = mongoose.model('Items', ItemsSchema)
