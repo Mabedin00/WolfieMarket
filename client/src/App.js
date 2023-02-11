196e190ae79789e08d28533692c29311950d5645
Po Yuen Lau <po.lau@stonybrook.edu>
2023-02-11 17:14:44 -0500

    sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import { React } from 'react'
-import { BrowserRouter, Route, Routes  } from 'react-router-dom'
-import LoginScreen from './components/LoginScreen'
-import './App.css';
-import { AuthContextProvider } from './auth';
-import { GlobalStoreContextProvider } from './store';
+import logo from "./logo.svg";
+import { React } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginScreen from "./components/LoginScreen";
+import SignUp from "./components/SignUp";
+import "./App.css";
+import { AuthContextProvider } from "./auth";
+import { GlobalStoreContextProvider } from "./store";
 
 function App() {
   return (
-        <BrowserRouter>
-            <AuthContextProvider>
-                <GlobalStoreContextProvider>              
-                    <Routes>
-                        <Route path="/login" exact element={<LoginScreen />} />
-                    </Routes>
-                </GlobalStoreContextProvider>              
-            </AuthContextProvider>
-        </BrowserRouter>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <GlobalStoreContextProvider>
+          <Routes>
+            <Route path="/login" exact element={<LoginScreen />} />
+            <Route path="/register" exact element={<SignUp />} />
+          </Routes>
+        </GlobalStoreContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,128 @@
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import logo from "../logo.svg";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Alert from "@mui/material/Alert";
+import Modal from "@mui/material/Modal";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Cancel";
+import AuthContext from "../auth";
+import { GlobalStoreContext } from "../store";
+
+export default function LoginScreen() {
+  const { auth } = useContext(AuthContext);
+  const { store } = useContext(GlobalStoreContext);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    console.log(auth.loggedIn);
+  };
+  return (
+    <div className="foundation-screen">
+      <img src={logo} className="app-logo" alt="logo" />
+      <div className="splash-text">
+        <Typography variant="h2" gutterBottom>
+          Sign Up
+        </Typography>
+      </div>
+      <Modal
+        open={auth.error !== null}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {/* Make a close button at the top right of the box */}
+          <IconButton sx={{ position: "absolute", top: "0", right: "0" }}>
+            <CloseIcon />
+          </IconButton>
+          <Alert severity="error">{auth.error}</Alert>
+        </Box>
+      </Modal>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          variant="filled"
+          name="username"
+          color="secondary"
+          autoComplete="username"
+          autoFocus
+        />{" "}
+        <br />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="email"
+          label="Email"
+          type="email"
+          variant="filled"
+          color="secondary"
+          id="password"
+          autoComplete="current-password"
+        />
+        <br />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          variant="filled"
+          color="secondary"
+          id="password"
+          autoComplete="current-password"
+        />
+        <br />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="confirmPassword"
+          label="Confirm Password"
+          type="confirmPassword"
+          variant="filled"
+          color="secondary"
+          id="password"
+          autoComplete="current-password"
+        />
+        <br />
+        <br />
+        <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+          Sign Up
+        </Button>{" "}
+        <br />
+        <Link to="/login/" variant="body2">
+          {"Have an account? Sign in"}
+        </Link>
+      </Box>
+    </div>
+  );
+}
