3141d1f993d9a0f51662859cd1337e0e4cc8f2e0
Po Yuen Lau <po.lau@stonybrook.edu>
2023-02-11 13:39:17 -0500

    added globals

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,2 +1,8 @@
 import { createContext, useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
+export const GlobalStoreContext = createContext({});
+export const GlobalStoreActionType = {
+  CHANGE_LISTING_NAME: "CHANGE_LISTING_NAME",
+  CLOSE_CURRENT_LISTING: "CLOSE_CURRENT_LISTING",
+  CREATE_NEW_LISTING: "CREATE_NEW_LISTING",
+};
