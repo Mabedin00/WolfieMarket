2dbc714f8257aff5e0a4c5639b697b198e026359
Tyler <tylhuang@umich.edu>
2023-02-12 01:37:24 -0500

    push

--- scripts/items.js
+++ scripts/items.js
@@ -1,12 +1,23 @@
-var checked = 0;
+var filter = '';
 
 window.addEventListener('load', (event) => {
     const checkboxes = document.getElementsByClassName('form-check-input');
     for (let i = 0; i < checkboxes.length; i++) {
         checkboxes[i].addEventListener('change', (event) => {
-            var items = document.getElementsByClassName('item');
             if (event.currentTarget.checked) {
-                console.log(event.currentTarget.id);
+                filter += '.' + event.currentTarget.id
+                var items = document.querySelectorAll('.item:not(' + filter + ')');
+                console.log(items);
+                for (let i = 0; i < items.length; i++) {
+                    items[i].style.display = 'none';
+                }
+            }
+            else {
+                filter = filter.replace('.' + event.currentTarget.id, '');
+                var items = filter === '' ? document.querySelectorAll('.item') : document.querySelectorAll(filter);
+                for (let i = 0; i < items.length; i++) {
+                    items[i].style.display = '';
+                }
             }
         });
     }
